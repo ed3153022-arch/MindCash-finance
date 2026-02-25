bafd60e48996fce0ab38b1d2e99bf75b5ef356cb
ed3153022-arch <ed3153022@gmail.com>
2026-02-25 20:35:07 -0300

    Update login page header texte

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
                 </div>
 
                 <h2 className="text-2xl font-bold text-white">
-                  Entrar na plataforma
+                  teste 999
                 </h2>
 
                 <p className="text-sm text-gray-400">
